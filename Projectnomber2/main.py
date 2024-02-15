32e156b1ac13a7494cb980b1c79c05e028ab7c21
Ura68504 <urahoruzenko314@gmail.com>
2024-02-15 21:33:17 +0300

    First Commit
    First Commit

---
+++ Projectnomber2/main.py
@@ -0,0 +1,7 @@
+user_name = str(input('Ведіть своє імя: '))
+
+user_age = int(input('Ведіть свій вік: '))
+
+user_number = input('Ведіть свій  номер телефона: ')
+
+print('Доброго дня ',user_name,', Вам ',user_age,' років, ваш номер телефона - ',user_number)
